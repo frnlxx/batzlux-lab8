827c767f6b322a8c8cdf42378033c91c429ffc7d
Francis <francislux@70.102.20.172.s.wireless.uri.edu>
2022-11-29 13:16:32 -0500

    Main complete

--- rumdump/src/main.rs
+++ rumdump/src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+use rumdump::rumdis;
+use rumdump::rumload;
 fn main() {
-    println!("Hello, world!");
+    let input = env::args().nth(1);
+    let instructions = rumload::load(input.as_deref());
+    println!("{} instructions", instructions.len());
+    for instruction in instructions {
+        println!("{}", rumdis::disassemble(instruction))
+    }
 }
