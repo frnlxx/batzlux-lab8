929437346756567c240cc281e417176388151280
Francis Batz-lux <60365302+frnlxx@users.noreply.github.com>
2023-03-22 14:05:58 -0400

    Delete rumdump directory

--- rumdump/target/debug/deps/rumdump-593c645fefbc4c01.d
+++
@@ -1,5 +0,0 @@
-/Users/francislux/rumdump/target/debug/deps/rumdump-593c645fefbc4c01.rmeta: src/main.rs
-
-/Users/francislux/rumdump/target/debug/deps/rumdump-593c645fefbc4c01.d: src/main.rs
-
-src/main.rs:
--- rumdump/target/debug/deps/rumdump-d12303982cde8bda.d
+++
@@ -1,5 +0,0 @@
-/Users/francislux/rumdump/target/debug/deps/rumdump-d12303982cde8bda.rmeta: src/main.rs
-
-/Users/francislux/rumdump/target/debug/deps/rumdump-d12303982cde8bda.d: src/main.rs
-
-src/main.rs:
